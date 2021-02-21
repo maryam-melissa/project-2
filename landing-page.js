b6a438604c2ffbb45edb200ddd9e12e31e7cbef2
maryam <maryum_shahnawaz@yahoo.com>
2021-02-21 13:49:25 -0500

    added eventlistener to landing page to redirect it to index page

---
+++ landing-page.js
@@ -0,0 +1,6 @@
+const landingPage = {};
+const button = document.querySelector('.start-btn');
+button.addEventListener('click', function (event) {
+  // Simulate an HTTP redirect:
+  window.location.replace("http://127.0.0.1:5501/index.html");
+});
