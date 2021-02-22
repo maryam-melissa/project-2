fdd8494d78b2257609d61e0cc9df986741daf2a9
Melissa Locquiao <ml13sj@brocku.ca>
2021-02-21 19:19:54 -0500

    get user input and genre functionality

--- index.js
+++ index.js
@@ -4,6 +4,53 @@ moviePicker.apiKey = `7b4ee228270013c7be42d484778165ef`;
 
 //Get Genre List form API call
 moviePicker.apiGenrelUrl = "https://api.themoviedb.org/3/genre/movie/list";
+//Get Movies with Actor from API call
+moviePicker.apiActorUrl = "https://api.themoviedb.org/3/search/person";
+
+moviePicker.apiFilteredMovies = "https://api.themoviedb.org/3/discover/movie";
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function (event) {
+  //prevents page from refreshing
+  event.preventDefault();
+
+  //todo
+  //get actor input
+  const actorInput = document.querySelector("input[type=text]");
+  // console.log(actorInput.value);
+  //get genre option
+  const genreInput = document.querySelector("select");
+  // console.log(genreInput.selectedOptions[0].value);
+
+  //api call
+  moviePicker.getActorId(actorInput.value, genreInput.selectedOptions[0].value);
+ 
+});
+
+moviePicker.getActorId = (name, genre) => {
+  const url = new URL(moviePicker.apiActorUrl);
+  url.search = new URLSearchParams({
+    api_key: moviePicker.apiKey,
+    query: name
+  })
+  fetch(url)
+  //parse response into JSON and return response so it can be used
+  .then((response) => {
+    return response.json();
+  })
+  //parse JSON promise response
+  .then((jsonResponse) => {
+    console.log(jsonResponse.results[0].id); 
+   moviePicker.getFilteredMovies(jsonResponse.results[0].id, genre);
+  })
+}
+
+//method to call api filtered with user input
+moviePicker.getFilteredMovies = (actorId, genre) => {
+  console.log(actorId, genre);
+
+}
 
 //method to call genre api and fetch data
 moviePicker.getGenre = () => {
@@ -21,6 +68,8 @@ moviePicker.getGenre = () => {
     })
     //parse JSON promise response
     .then((jsonResponse) => {
+      // console.log(jsonResponse); 
+
       //use the data to load into genre dropdown
       moviePicker.loadGenreData(jsonResponse.genres);
     })
@@ -44,10 +93,13 @@ moviePicker.loadGenreData = (genres) => {
   });
 }
 
+
+
 //app initialize method
 moviePicker.init = () => {
   //genre api call to load genre data to form
   moviePicker.getGenre();
+
 }
 
 //method call to start app
